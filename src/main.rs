0f9b2800705f4a512167d58be8070d2eb1e9ea4d
Tanishq Dubey <tanishq.dubey@gmail.com>
2020-06-20 20:46:32 -0400

    basic prompt

---
+++ src/main.rs
@@ -0,0 +1,29 @@
+use std::io::{self, Write};
+
+fn read_input() -> String {
+    let mut input_string: String = String::new();
+    std::io::stdin().read_line(&mut input_string).ok().expect("Could not read input");
+    return input_string.trim().to_string();
+}
+
+fn lex(input: String) -> String {
+    return input
+}
+
+fn expand(input: String) -> String {
+    return input
+}
+
+fn execute(input: String) -> i32{
+    return 0
+}
+
+fn main() {
+    loop {
+        print!("$> ");
+        io::stdout().flush().unwrap();
+        let input_string: String = read_input();
+        let retval: i32 = execute(expand(lex(input_string)));
+        println!("{}", retval);
+    }
+}
